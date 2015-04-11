a9755fcd7e0593d30e4a71d89ee1d6a67ae2dbf3
root <root@hunter.(none)>
2015-04-11 20:43:58 +0800

    testdbix

---
+++ testdbixcustom.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: test.pl
+#
+#        USAGE: ./test.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 04/11/2015 08:27:03 PM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+use DBIx::Custom;
+use Data::Dumper;
+
+my $dbi = DBIx::Custom->connect(
+    dsn => "dbi:mysql:database=hunter:localhost:3306",
+    user => "root",
+    password => "hunter1983907",
+    option => {
+        mysql_enable_utf8 => 1
+    }
+);
+
+my $result = $dbi->execute(
+    "select id from q_query where title = :title",
+    {
+        title => 'github',
+    }
+);
+
+my $row = $result->one;
+
+print $row->{id};
