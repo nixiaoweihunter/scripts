2fa67717b72c540262eacd540548aabba87671b6
nixiaoweihunter <nixiaoweihunter@hotmail.com>
2017-04-21 17:31:16 +0800

    add weblogin

---
+++ python/webautologin.py
@@ -0,0 +1,24 @@
+#!/root/python27/bin/python
+# -*- coding: utf-8 -*-
+import cookielib
+import urllib
+import urllib2
+
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+index_url = "http://zabbix.ymm56.com/index.php"
+
+req = urllib2.Request( index_url, urllib.urlencode( {
+                    'autologin': 1,
+                    'enter': 'Sign in',
+                    'name': '',
+		    'password': ''
+                    } ) )
+
+response = opener.open( req )
+html = response.read().decode( 'utf-8' )
+print html
+
+
+
+
